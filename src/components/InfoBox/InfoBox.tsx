fa365c8740bd23d1e0274f1c4e55dbbd4de45569
helenebehrenssvv <helene.randi.behrens@vegvesen.no>
2025-04-28 19:25:23 +0200

    Resetter oppgaver

--- src/components/InfoBox/InfoBox.tsx
+++ src/components/InfoBox/InfoBox.tsx
@@ -1,5 +1,5 @@
 export const InfoBox = ({infoText}:{infoText: string}) => {
-    return (<p className="text-primary bg-secondary font-extrabold text-5xl">
+    return (<p className="text-primary bg-secondary text-5xl">
         {infoText}
     </p>);
 }
