8a98c724b67653e0225d4b215db8677ca1c0793d
helenebehrenssvv <helene.randi.behrens@vegvesen.no>
2025-05-11 18:36:30 +0200

    Endrer index igjen

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export * from "./oppgaver/Oppgave1.tsx";
-export * from './utils/tailwind-content.ts';
+import InfoBox from "./oppgaver/Oppgave1.tsx";
+
+export default InfoBox  // Må det være export default?
+//export * from './utils/tailwind-content.ts';
