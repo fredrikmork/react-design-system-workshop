77e34332348c509c112964d4cff8845a389667e9
helenebehrenssvv <helene.randi.behrens@vegvesen.no>
2025-05-11 22:47:39 +0200

    Oppgave 7

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import InfoBox from "./oppgaver/Oppgave7.tsx";
 
-export default InfoBox  // Må det være export default?
-//export * from './utils/tailwind-content.ts';
+export default InfoBox
--- src/oppgaver/Oppgave7.tsx
+++ src/oppgaver/Oppgave7.tsx
@@ -1,10 +1,12 @@
 // Publiser infoboksen din som en pakke
 
-// I arbeidet med workshopen fant vi ut at selv om tailwind er kult, er det ikke så egnet til et designsystem
-// For å gjøre det enklere for oss selv lager vi en ny infoboks-komponent som vi styler med vanlig css
+// Så langt i workshopen har vi brukt tailwind til styling, men for å gjøre det litt lettere for oss selv, går vi over
+// til vanlig css når vi publiserer komponenten vår
+// Vi tar utgangspunkt i en ny InfBox-komponent, og målet med Oppgave 7 er å publisere designsystemet med denne som
+// en pakke og få en nabogruppe til å ta den i bruk
 
 // Oppgave 7.1:
-// Hvis du vil: bruk litt tid på å gjøre infoboks-komponenten enda finere enn den allerede er:)
+// Hvis du vil: bruk litt tid på å gjøre infoboks-komponenten under enda finere enn den allerede er:)
 
 import './Oppgave7.css'
 
@@ -19,20 +21,39 @@ const InfoBox = ({infoText}:{infoText: string}) => {
 }
 export default InfoBox
 
-// Oppgave 7.2 - publiser Infoboksen som en pakke til npm og få en nabogruppe til å ta den i bruk!
+// Nå: publiser Infoboksen som en pakke til npm og få en nabogruppe til å ta den i bruk!
 // Her kan dere godt gå sammen med en annen gruppe og samarbeide med dem
 
 // Vi har jukset litt og satt opp prosjektet sånn at det skal kunne publiseres rett til npm.
 // Her kan du lese litt om hvordan vi konfigurerte prosjektet for å gjøre det klar for publisering:
 // https://medium.com/@kaljessy/publish-your-react-library-to-npm-using-vite-136dc81e368d
 
-// ---  Publiser pakken til npm   ---
-// Ta utgangspunkt i guiden over og guiden fra forrige oppgave, og se om du får til å publisere designsystemet med infoboksen til npm
+// ---  Oppgave 7.2 - Publiser pakken til npm   ---
+// Ta utgangspunkt i guiden over og guiden fra forrige oppgave, og se om du får til å publisere designsystemet
+// med infoboksen til npm
 // Et par ting du kan tenke på:
 // * Husk å oppdatere navnet på pakken din
 // * I forrige workshop brukte vi tsup til å bygge pakken, denne gangen bruker vi vite. Er det noen forskjeller?
 
-// --- Ta en annen gruppe sin kompnent i bruk ---
+// Kort oppsummert har vi gjort dette:
+// * Opprett en fil index.ts under source, og eksporter infoboksen fra den
+// * Konfigurer bygging av pakken i vite.config.ts:
+//    * Legg til src/index.ts som entry path for pakken
+//    * Legg til pakkenavnet som navnet på libet som bygges
+//    * En del annet snacks som du kan finne mer om her:
+//       https://medium.com/@kaljessy/publish-your-react-library-to-npm-using-vite-136dc81e368d
+// * Oppdater package.json med blant annet:
+//    * Navn på pakken
+//    * Om pakken skal være public eller private
+//    * Beskrivelse av pakken
+//    * Hvor i pakken koden finnes (/dist)
+//    * Et par andre ting
+// * Oppdater tsconfig.json for å legge til støtte for typescript:
+//    * Legg til "include": ["src", "./index.ts"]
+//    * Legg til "typeRoots": ["./dist/index.d.ts", "node_modules/@types"] i compilerOptions for å fortelle hvor
+//      TypeScript finner typedefinisjoner
+
+// --- Oppgave 7.2 - Ta en annen gruppe sin kompnent i bruk ---
 // 1. Last ned pakken fra en av de andre gruppene fra npm med
 //   npm install <pakkenavn>
 // 2. Ta pakken i bruk, for eksempel I App.tsx med
