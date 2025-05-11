92f7e8f2357283399e5f2632267a041cc8cc3815
helenebehrenssvv <helene.randi.behrens@vegvesen.no>
2025-05-11 20:07:48 +0200

    Oppgave 7

--- src/oppgaver/Oppgave7.tsx
+++ src/oppgaver/Oppgave7.tsx
@@ -1,6 +1,10 @@
 // Publiser infoboksen din som en pakke
 
-// Bruk guiden fra forrige del av workshopen, og se om du får til å publisere infoboksen som en pakke!@
+// I arbeidet med workshopen fant vi ut at selv om tailwind er kult, er det ikke så egnet til et designsystem
+// For å gjøre det enklere for oss selv lager vi en ny infoboks-komponent som vi styler med vanlig css
+
+// Oppgave 7.1:
+// Hvis du vil: bruk litt tid på å gjøre infoboks-komponenten enda finere enn den allerede er:)
 
 import './Oppgave7.css'
 
@@ -10,11 +14,19 @@ const InfoBox = ({infoText}:{infoText: string}) => {
             <p className="infobox">
                 {infoText}
             </p>
-            <p className="infobox">
-                {infoText}
-            </p>
         </>
     );
 }
-
 export default InfoBox
+
+// Oppgave 7.2 - publiser Infoboksen som en pakke til npm og få en nabogruppe til å ta den i bruk!
+
+// Vi har jukset litt og satt opp prosjektet sånn at det skal kunne publiseres rett til npm.
+// Her kan du lese litt om hvordan vi konfigurerte prosjektet for å gjøre det klar for publisering:
+// https://medium.com/@kaljessy/publish-your-react-library-to-npm-using-vite-136dc81e368d
+
+// ---  Publiser pakken til npm   ---
+// Ta utgangspunkt i guiden over og guiden fra forrige oppgave, og se om du får til å publisere designsystemet med infoboksen til npm
+// Et par ting du kan tenke på:
+// * Husk å oppdatere navnet på pakken din
+// * I forrige workshop brukte vi tsup til å bygge pakken, denne gangen bruker vi vite. Er det noen forskjeller?
