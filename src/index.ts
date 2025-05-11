84b3f6ad1719ff8e32993d8f8ae1a4788e07421a
helenebehrenssvv <helene.randi.behrens@vegvesen.no>
2025-05-11 18:14:39 +0200

    Forsøk med tailwind-content i utils

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import InfoBox from "./oppgaver/Oppgave1.tsx";
+export * from './utils/tailwind-content.ts';
 
 export default InfoBox  // Må det være export default?
---
+++ src/utils/tailwind-content.ts
@@ -0,0 +1,5 @@
+// src/utils/my-tailwind-content.ts
+
+export function tailwindContent() {
+    return './node_modules/helenes-desginsystem/dist/**/*.js'; // burde det vært .ts?
+}
